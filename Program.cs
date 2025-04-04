a3cdd24427ad06767cf8bc6bfc03c2f356a97255
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 20:30:39 -0300

    josé

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+double b,expoente,potencia;
+
+Console.WriteLine("calculadora de potencia");
+
+Console.Write("base:");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("expoente:");
+expoente = Convert.ToDouble(Console.ReadLine());
+
+potencia = Math.Pow(b,expoente);
+
+Console.WriteLine($"resultado:{potencia:N2}");
